9e2b60aff93a611f747c0b52328f06cd12541645
ssar85 <ssar85@mail.ru>
2021-01-07 15:53:36 +0500

    Декомпилировал, изменил код и создал новую сборку

---
+++ Lesson7-1/decompilation/Lesson7/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lesson7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson7")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9b3f745c-0d48-45eb-b9be-720fd578baf3")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
